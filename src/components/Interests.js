467c2774718fed795379bc46aabb93bee13e9f25
cloudflips32 <64573965+cloudflips32@users.noreply.github.com>
2021-07-11 12:43:22 -0400

    final media polishing

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -6,23 +6,23 @@ const Interests = () => {
       <div className="section" id="interests">
         <h2>My Interests</h2>
         <div className="interests-group">
-          <h3><span>Healthy</span> Eating</h3>
           <div className="item">
+            <h3><span>Healthy</span> Eating</h3>
             <img src="images/sushi.jpg" alt="sushi plate" />
           </div>
-          <h3><span>Working</span> Out</h3>
           <div className="item">
+            <h3><span>Working</span> Out</h3>
             <img src="images/deadlift.jpg" alt="olympic bar 130lb" />
           </div>
         </div>
 
         <div className="interests-group">
-          <h3><span>Jeopardy</span>- RIP Alex!</h3>
           <div className="item">
+            <h3><span>Jeopardy</span></h3>
             <img src="images/jeopardy.jpg" alt="Jeopardy Board" />
           </div>
-          <h3><span>E-Sports</span></h3>
           <div className="item">
+            <h3><span>E-Sports</span></h3>
             <img src="images/esports.jpg" alt="E-Sports stadium crowd" />
           </div>
         </div>
